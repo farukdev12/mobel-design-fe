b3628a8b78b78def26fb160e3d02ce43f62d7670
Faruk <89403617+FarukCakir1@users.noreply.github.com>
2025-10-26 12:24:42 +0300

    feat: photos added

---
+++ src/constants/GalleryItems.ts
@@ -0,0 +1,12 @@
+export const galleryItems = [
+    {
+        id: 1,
+        title: "El Yapımı Seramik Kupa",
+        cover: "/images/landing-slider1.jpg",
+        images: [
+            "/images/landing-slider2.jpg",
+            "/images/landing-slider3.jpg",
+            "/images/landing-slider1.jpg"
+        ],
+    },
+]
